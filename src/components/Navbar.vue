8ed24631ae14f76c79ee517f8865eb24f755405a
z-p-c <1480325048@qq.com>
2020-09-21 14:12:22 +0800

    第一次提交

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <el-col :span="24">
+        <el-menu default-active="2"
+                 class="el-menu-vertical-demo"
+                 background-color="#545c64"
+                 text-color="#fff"
+                 active-text-color="#ffd04b">
+            <el-menu-item index="0">
+                <i class="el-icon-menu"></i>
+                <span slot="title">首页</span>
+            </el-menu-item>
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>系统设置</span>
+                </template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+                <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>商城管理</span>
+                </template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+                <el-menu-item index="2-3">选项3</el-menu-item>
+            </el-submenu>
+        </el-menu>
+    </el-col>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped></style>
